f1a9ec555f3149a88b689a17e03e48bbc856e1cb
Er1k-sav <71546251+Er1k-sav@users.noreply.github.com>
2021-03-11 20:42:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ window.onload = () => {
     const app = document.querySelector("#app");
     const startBT = document.querySelector("#startBT");
     const searchInput = document.querySelector("#searchInput");
+    const visualieBT = document.querySelector("#visualizeBT");
     const suggestions = document.querySelector(".suggestions");
+    const source = document.querySelector("#source");
+    const ballTitle = document.querySelector("#ballTitle");
+    const arrow = document.querySelector("#arrowContainer");
     var criterion = "";
     var nodesNum = 6;
     var linksNum = 0;
@@ -64,8 +68,8 @@ window.onload = () => {
             node.id = "node" + id;
             node.className = "nodeClass";
             circle.id = "circle";
-            title.id = "ballTitle";
-            title.innerHTML = "";
+            title.id = "linkTitle";
+            title.innerHTML = "Er1k-sav/ArraySorting";
 
             viewArea.appendChild(node);
             node.appendChild(circle);
@@ -78,49 +82,70 @@ window.onload = () => {
             const node5 = document.querySelector("#node4");
             const node6 = document.querySelector("#node5");
             const style = getComputedStyle(source);
+            let n1top = parseInt(style.top) - 70 + "px";
+            let n1Left = parseInt(style.left) + 1 + "px";
+            let n2top = parseInt(style.top) - 40 + "px";
+            let n2left = parseInt(style.left) - 68 + "px";
+            let n3top = parseInt(style.top) - 40 + "px";
+            let n3left = parseInt(style.left) + 68 + "px";
+            let n4top = parseInt(style.top) + 40 + "px";
+            let n4left = parseInt(style.left) - 68 + "px";
+            let n5top = parseInt(style.top) + 40 + "px"; 
+            let n5left = parseInt(style.left) + 68 + "px";
+            let n6top = parseInt(style.top) + 70 + "px";
+            let n6left = parseInt(style.left) + 1 + "px";
             if (id === 1) {
-                node1.style.top = parseInt(style.top) - 70 + "px";
-                node1.style.left = parseInt(style.left) + "px";
+                node1.style.top = n1top;
+                node1.style.left = n1Left;
             } else if (id === 2) {
-                node1.style.top = parseInt(style.top) - 70 + "px";
-                node1.style.left = parseInt(style.left) + "px";
-                node2.style.top = parseInt(style.top) - 40 + "px";
-                node2.style.left = parseInt(style.left) - 68 + "px";
+                node1.style.top = n1top;
+                node1.style.left = n1Left;
+                node2.style.top = n2top;
+                node2.style.left = n2left;
             } else if (id === 3) {
-                node1.style.top = parseInt(style.top) - 70 + "px";
-                node1.style.left = parseInt(style.left) + "px";
-                node2.style.top = parseInt(style.top) - 40 + "px";
-                node2.style.left = parseInt(style.left) - 68 + "px";
-                node3.style.top = parseInt(style.top) - 40 + "px";
-                node3.style.left = parseInt(style.left) + 68 + "px";
+                node1.style.top = n1top;
+                node1.style.left = n1Left;
+                node2.style.top = n2top;
+                node2.style.left = n2left;
+                node3.style.top = n3top;
+                node3.style.left = n3left;
             } else if (id === 4) {
-                node1.style.top = parseInt(style.top) - 70 + "px";
-                node1.style.left = parseInt(style.left) + "px";
-                node2.style.top = parseInt(style.top) - 40 + "px";
-                node2.style.left = parseInt(style.left) - 68 + "px";
-                node3.style.top = parseInt(style.top) - 40 + "px";
-                node3.style.left = parseInt(style.left) + 68 + "px";
-                node4.style.left = parseInt(style.left) - 68 + "px";
-                node4.style.top = parseInt(style.top) + 40 + "px"; 
+                node1.style.top = n1top;
+                node1.style.left = n1Left;
+                node2.style.top = n2top;
+                node2.style.left = n2left;
+                node3.style.top = n3top;
+                node3.style.left = n3left;
+                node4.style.left = n4left;
+                node4.style.top = n4top; 
                 id = 5;
             } else if (id === 5) {
-                console.log("LOL")
-                node1.style.top = parseInt(style.top) - 70 + "px";
-                node1.style.left = parseInt(style.left) + "px";
-                node2.style.top = parseInt(style.top) - 40 + "px";
-                node2.style.left = parseInt(style.left) - 68 + "px";
-                node3.style.top = parseInt(style.top) - 40 + "px";
-                node3.style.left = parseInt(style.left) + 68 + "px";
-                node4.style.left = parseInt(style.left) - 68 + "px";
-                node4.style.top = parseInt(style.top) + 40 + "px"; 
-                node5.style.left = parseInt(style.left) + 68 + "px";
-                node5.style.top = parseInt(style.top) + 40 + "px"; 
-                node6.style.top = parseInt(style.top) + 70 + "px";
-                node6.style.left = parseInt(style.left) + "px";
+                node1.style.top = n1top;
+                node1.style.left = n1Left;
+                node2.style.top = n2top;
+                node2.style.left = n2left;
+                node3.style.top = n3top;
+                node3.style.left = n3left;
+                node4.style.left = n4left;
+                node4.style.top = n4top; 
+                node5.style.left = n5left;
+                node5.style.top = n5top; 
+                node6.style.top = n6top;
+                node6.style.left = n6left;
             }
         }
     }
-    tree(document.querySelector("#source"));
 
+    var visualize = () => {
+        visualieBT.addEventListener("click", () => {
+            let value = searchInput.value.toLowerCase();
+            criterion = value;
+            ballTitle.innerHTML = criterion;
+            tree(source);
+            source.style.opacity = 1;
+            setTimeout(() => {arrow.style.opacity = 1}, 1500)
+        })
+    }
+    visualize();
     startVisualization();
-}
+}vv
