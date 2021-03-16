6a76445a9a819c3bc326faed6d983219ca6c3688
Er1k-sav <71546251+Er1k-sav@users.noreply.github.com>
2021-03-16 18:20:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ window.onload = () => {
     const source = document.querySelector("#source");
     const ballTitle = document.querySelector("#ballTitle");
     const arrow = document.querySelector("#arrowContainer");
+    const arrow1 = document.querySelector("#arrow1");
+    const arrow2 = document.querySelector("#arrow2");
+    const arrow3 = document.querySelector("#arrow3");
+    const arrow4 = document.querySelector("#arrow4");
+    const arrow5 = document.querySelector("#arrow5");
+    const arrow6 = document.querySelector("#arrow6");
+    const numOfNodesInput = document.querySelector("#numOfNodes");
     var criterion = "";
     var nodesNum = 6;
     var linksNum = 0;
@@ -64,16 +71,30 @@ window.onload = () => {
             let node = document.createElement("div");
             let circle = document.createElement("div");
             let title = document.createElement("a");
+            let box = document.createElement("div");
+            let pfp = document.createElement("div");
+            let name = document.createElement("div");
+            let description = document.createElement("div");
             
             node.id = "node" + id;
             node.className = "nodeClass";
             circle.id = "circle";
             title.id = "linkTitle";
             title.innerHTML = "Er1k-sav/ArraySorting";
+            box.className = "classBox";
+            box.id = "box" 
+            pfp.id = "pfp";
+            name.id = "name";
+            name.innerHTML = "Er1k-sav/arraySorting";
+            description.id = "description";
 
             viewArea.appendChild(node);
             node.appendChild(circle);
             node.appendChild(title);
+            box.appendChild(pfp);
+            box.appendChild(name);
+            box.appendChild(description);
+            node.appendChild(box);
 
             const node1 = document.querySelector("#node0");
             const node2 = document.querySelector("#node1");
@@ -94,14 +115,33 @@ window.onload = () => {
             let n5left = parseInt(style.left) + 68 + "px";
             let n6top = parseInt(style.top) + 70 + "px";
             let n6left = parseInt(style.left) + 1 + "px";
-            if (id === 1) {
+            if (id === 0) {
                 node1.style.top = n1top;
                 node1.style.left = n1Left;
+                arrow1.style.opacity = 1;
+                node1.style.opacity = 1;
+            } else if (id === 1) {
+                node1.style.top = n1top;
+                node1.style.left = n1Left;
+                node2.style.top = n2top;
+                node2.style.left = n2left;
+                arrow1.style.opacity = 1;
+                arrow2.style.opacity = 1;
+                node1.style.opacity = 1;
+                node2.style.opacity = 1;
             } else if (id === 2) {
                 node1.style.top = n1top;
                 node1.style.left = n1Left;
                 node2.style.top = n2top;
                 node2.style.left = n2left;
+                node3.style.top = n3top;
+                node3.style.left = n3left;
+                arrow1.style.opacity = 1;
+                arrow2.style.opacity = 1;
+                arrow3.style.opacity = 1;
+                node1.style.opacity = 1;
+                node2.style.opacity = 1;
+                node3.style.opacity = 1;
             } else if (id === 3) {
                 node1.style.top = n1top;
                 node1.style.left = n1Left;
@@ -109,6 +149,17 @@ window.onload = () => {
                 node2.style.left = n2left;
                 node3.style.top = n3top;
                 node3.style.left = n3left;
+                node4.style.left = n4left;
+                node4.style.top = n4top; 
+                arrow1.style.opacity = 1;
+                arrow2.style.opacity = 1;
+                arrow3.style.opacity = 1;
+                arrow4.style.opacity = 1;
+                node1.style.opacity = 1;
+                node2.style.opacity = 1;
+                node3.style.opacity = 1;
+                node4.style.opacity = 1;
+                id = 4
             } else if (id === 4) {
                 node1.style.top = n1top;
                 node1.style.left = n1Left;
@@ -118,7 +169,18 @@ window.onload = () => {
                 node3.style.left = n3left;
                 node4.style.left = n4left;
                 node4.style.top = n4top; 
-                id = 5;
+                node5.style.left = n5left;
+                node5.style.top = n5top; 
+                arrow1.style.opacity = 1;
+                arrow2.style.opacity = 1;
+                arrow3.style.opacity = 1;
+                arrow4.style.opacity = 1;
+                arrow5.style.opacity = 1;
+                node1.style.opacity = 1;
+                node2.style.opacity = 1;
+                node3.style.opacity = 1;
+                node4.style.opacity = 1;
+                node5.style.opacity = 1;
             } else if (id === 5) {
                 node1.style.top = n1top;
                 node1.style.left = n1Left;
@@ -132,8 +194,52 @@ window.onload = () => {
                 node5.style.top = n5top; 
                 node6.style.top = n6top;
                 node6.style.left = n6left;
+                arrow1.style.opacity = 1;
+                arrow2.style.opacity = 1;
+                arrow3.style.opacity = 1;
+                arrow4.style.opacity = 1;
+                arrow5.style.opacity = 1;
+                arrow6.style.opacity = 1;
+                node1.style.opacity = 1;
+                node2.style.opacity = 1;
+                node3.style.opacity = 1;
+                node4.style.opacity = 1;
+                node5.style.opacity = 1;
+                node6.style.opacity = 1;
+            } else if (id === 6) {
+                node1.style.top = n1top;
+                node1.style.left = n1Left;
+                node2.style.top = n2top;
+                node2.style.left = n2left;
+                node3.style.top = n3top;
+                node3.style.left = n3left;
+                node4.style.left = n4left;
+                node4.style.top = n4top; 
+                node5.style.left = n5left;
+                node5.style.top = n5top; 
+                node6.style.top = n6top;
+                node6.style.left = n6left;
+                arrow1.style.opacity = 1;
+                arrow2.style.opacity = 1;
+                arrow3.style.opacity = 1;
+                arrow4.style.opacity = 1; 
+                arrow5.style.opacity = 1; 
+                arrow6.style.opacity = 1;
+                node1.style.opacity = 1;
+                node2.style.opacity = 1;
+                node3.style.opacity = 1;
+                node4.style.opacity = 1;
+                node5.style.opacity = 1;
+                node6.style.opacity = 1;
             }
         }
+    }
+
+    var getNumOfNodes = () => {
+        numOfNodesInput.addEventListener("input", () => {
+            let value = numOfNodesInput.value.toLowerCase();
+            nodesNum = value;
+        })
     }
 
     var visualize = () => {
@@ -143,9 +249,14 @@ window.onload = () => {
             ballTitle.innerHTML = criterion;
             tree(source);
             source.style.opacity = 1;
-            setTimeout(() => {arrow.style.opacity = 1}, 1500)
+            fetch('https://api.github.com/search/repositories?q=' + criterion) 
+                .then(response => response.json())
+                .then(data => {
+                    console.log(JSON.stringify(data))
+                })
         })
     }
     visualize();
+    getNumOfNodes();
     startVisualization();
 }
